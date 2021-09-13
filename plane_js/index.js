d5872e3b29bb44a0f61d9a63f4fae3cebfeb0293
kentaro-hirano <yakyukentaro@gmail.com>
2021-09-14 00:35:57 +0900

    create complete-area

--- plane_js/index.js
+++ plane_js/index.js
@@ -1,78 +1,8 @@
+import React from "react";
+import ReactDom from "react-dom";
 
-const onClickAdd = () => {
-  // テキストボックスの値を取得し、初期化する
-  const inputText = document.getElementById("add-text").value;
-  document.getElementById("add-text").value = "";
-
-  createIncompleteList(inputText);
-  
+const App = () => {
+  return <h1>こんにちは！</h1>;
 };
 
-// 未完了リストに追加する関数
-const createIncompleteList = (text) => {
-  // div生成
-  const div = document.createElement("div");
-  div.className = "list-row";
-
-  // liタグ生成
-  const li = document.createElement("li");
-  li.innerText = text;
-
-  // button(完了)タグ生成
-  const completeButton = document.createElement("button");
-  completeButton.innerText = "完了";
-  completeButton.addEventListener("click", () => {
-    // 押された完了ボタンの親タグ(div)を未完了リストから削除
-    deleteFromoIncompleteList(completeButton.parentNode);
-    // 完了リストに追加する要素
-    const addTarget = completeButton.parentNode;  
-    // TODO内容テキストを取得
-    const text = addTarget.firstElementChild.innerText;
-    // div以下を初期化
-    addTarget.textContent = null;
-    // liタグを生成
-    const li = document.createElement("li");
-    li.innerText = text;
-    // buttonタグ生成
-    const backButton = document.createElement("button");
-    backButton.innerText = "戻す";
-    backButton.addEventListener("click", () => {
-      // 押された戻すボタンの親タグ(div)を完了リストから削除
-      const deleteTarget = backButton.parentNode;
-      document.getElementById("complete-list").removeChild(deleteTarget); 
-
-      // テキスト取得
-      const text = backButton.parentNode.firstElementChild.innerText;
-      createIncompleteList(text);
-    });
-    // divタグの子要素に各要素をを設定
-    addTarget.appendChild(li);
-    addTarget.appendChild(backButton);
-    document.getElementById("complete-list").appendChild(addTarget);
-  });
-  
-  // button(削除)タグ生成
-  const deleteButton = document.createElement("button");
-  deleteButton.innerText = "削除";
-  deleteButton.addEventListener("click", () => {
-    // 完了リストに追加する要素
-   
-    // 押された削除ボタンの親タグ(div)を未完了リストから削除
-    deleteFromoIncompleteList(deleteButton.parentNode);
-  });
-  
-  // divタグの子要素に各要素を指定
-  div.appendChild(li);
-  div.appendChild(completeButton);
-  div.appendChild(deleteButton);
-  
-  // 未完了リストに追加
-  document.getElementById("incomplete-list").appendChild(div);
-};
-
-// 未完了リストから指定の要素を削除
-const deleteFromoIncompleteList = (target) => {
-  document.getElementById("incomplete-list").removeChild(target);
-}
-
-document.getElementById("add-button").addEventListener("click", ()=> onClickAdd());
+ReactDom.render(<App />, document.getElementById("root"));
--- react-tut/src/App.jsx
+++ react-tut/src/App.jsx
@@ -1,40 +1,36 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
-
 const App = () => {
-  console.log("最初")
   const [num, setNum] = useState(0);
-  const [faceShowFlag, setFaceShowFlag] = useState(true);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
 
-  const onClicCountUp = () => {
+  const onClickCountUp = () => {
     setNum(num + 1);
   };
-
   const onClickSwitchShowFlag = () => {
     setFaceShowFlag(!faceShowFlag);
-  },;
-
+  };
+  
   useEffect(() => {
-    console.log("useEffect");
-  }, []);
-  if (num > 0) {
-    if (num % 3 === 0) {
-       faceShowFlag || setFaceShowFlag(true);
-    } else {
-      faceShowFlag && setFaceShowFlag(false); 
-    }
-  }
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }  
+  }, [num]);
 
   return (
     <>
-      <h1 style= {{ color: "red"}}>こんにちは！</h1>
-      <ColorfulMessage color= "blue" >お元気ですか？</ColorfulMessage>
-      <ColorfulMessage color= "pink" >元気です！</ColorfulMessage>
-      <button onClick= {onClicCountUp}>カウントアップ</button>
+      <h1 style= {{ color: "red" }}>こんにちは！</h1>
+      <ColorfulMessage color= "blue">お元気ですか？</ColorfulMessage> 
+      <ColorfulMessage color= "pink">お元気です</ColorfulMessage> 
+      <button onClick= {onClickCountUp}>カウントアップ</button>
       <br />
       <button onClick= {onClickSwitchShowFlag}>ON/OFF</button>
       <p>{num}</p>
-      {faceShowFlag && <p>(*^^*)</p>}      
+      {faceShowFlag && <p>(*^^*)</p>}
     </>
   );
 };
